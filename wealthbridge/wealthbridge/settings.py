cdc4e9ff702f5336022ecc43c7891ea1862f2a59
Rayflex189 <roryisaiah66@gmail.com>
2026-07-02 23:43:11 +0100

    Fix ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS to support non-www and wildcard domains

--- wealthbridge/wealthbridge/settings.py
+++ wealthbridge/wealthbridge/settings.py
@@ -50,7 +50,7 @@ SECRET_KEY = 'django-insecure-3_)^u&niz%-isn%ciqt+qx7*3h!bo(js3+s%x0qray8bkb8d_1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.stacktellertrust.online', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['www.stacktellertrust.online', 'stacktellertrust.online', '127.0.0.1', 'localhost', '*']
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'dlzn0moho',
@@ -60,6 +60,7 @@ CLOUDINARY_STORAGE = {
 
 CSRF_TRUSTED_ORIGINS = [
     "https://www.stacktellertrust.online",
+    "https://stacktellertrust.online",
     "http://127.0.0.1:8080",
     "http://localhost:8080",
 ]
